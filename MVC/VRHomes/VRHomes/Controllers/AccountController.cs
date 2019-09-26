1940461746bedbc85aecf7aa68f7bb8e53c19a89
MuhammadImdadK <muhammadimdadkhan1997@gmail.com>
2019-09-26 19:49:57 +0500

    images changing on Dropdown
    MVC Login only

---
+++ MVC/VRHomes/VRHomes/Controllers/AccountController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VRHomes.Models;
+using System.Data.SqlClient;
+namespace VRHomes.Controllers
+{
+    public class AccountController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        // GET: Account
+        public ActionResult Login()
+        {
+
+            return View();
+        }
+        void connectionString()
+        {
+            con.ConnectionString = @"Data Source=PHEECA\SQLEXPRESS2016;Initial Catalog=VRHomesDB;Integrated Security=True";
+        }
+        // GET: Account/Verify/5
+        [HttpPost]
+        public ActionResult Verify(Account acc)
+        {
+            connectionString();
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select * from login where username='"+acc.Name+"'and password='"+acc.Password+"'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+
+                con.Close();
+                return View("MainPage");
+            }
+            else
+            {
+
+                con.Close();
+                return View("LoginFailed");
+            }
+        }
+
+        // GET: Account/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Account/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Account/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Account/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Account/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Account/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
