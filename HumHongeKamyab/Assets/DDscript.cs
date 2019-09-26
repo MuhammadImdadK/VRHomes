f359e9a81fd3170bb70249bf197f7671c83e7079
MuhammadImdadK <muhammadimdadkhan1997@gmail.com>
2019-09-26 19:01:06 +0500

    Image changing on drop down selection

---
+++ HumHongeKamyab/Assets/DDscript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DDscript : MonoBehaviour
+{
+     public Dropdown Mdropdown;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        PopulateDropDown();
+    }
+    
+    
+    void PopulateDropDown()
+    {
+        List<string> DD = new List<string>(){"Room 1","Room 2","Room 3","Room 4","Room 5"};
+       Mdropdown.AddOptions(DD);
+         
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- HumHongeKamyab/Assets/sc.cs
+++ HumHongeKamyab/Assets/sc.cs
@@ -1,28 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class sc : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Material one;
-    public Material two;
-
+    public Dropdown Mdropdown;
+      public Material [] mat = new Material[5];
+   
+    Material matSetter ;
     void Start()
     {
-       
+        matSetter=mat[0];
+    
     }
 
   public void onClick()
     {
 
-        Update1();
+        Indexer();
         //RenderSettings.skybox = one;
     }
 
+    public void Indexer ()
+    {
+        matSetter=mat[Mdropdown.value];
+        Update1();
+        
+    }
     // Update is called once per frame
     void Update1()
     {
-        RenderSettings.skybox = two;
+        RenderSettings.skybox =matSetter;
     }
 }
